b8ddf34e0f15b35a1c32120be57ec6e8a0f6bf86
Wil <wfkoch@gmail.com>
2018-05-10 16:57:51 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# VidRecv
+
+VidRecv is a video receiver for receiving video from a camera sensor in Gazebo
+to provide first person view (FPV) in the [Betaloop](https://github.com/Aeroloop/betaloop) flight simulator.
+
+Essentially this has just been pulled from the Topic Viewer in Gazebo so it can
+be run without the overhead of the entire Gazebo GUI.
+
+# Build
+
+1. Create directory `build`
+2. `cd build`
+3. cmake ..
+4. `make` which will produce VidRecv
+
+
---
+++ main.cc
@@ -0,0 +1,34 @@
+#include <gazebo/gazebo_client.hh>
+#include <stdio.h>
+#include "gazebo/gui/qt.h"
+#include "ImageView.hh"
+#include "TopicView.hh"
+#include "ViewFactory.hh"
+
+int main(int argc, char **argv)  
+{
+	gazebo::client::setup(argc, argv);
+	QApplication app(argc, argv);
+
+	QWidget window;
+	window.resize(100, 100);
+	std::cout << "Creating window\n";
+	std::string msgType = "gazebo.msgs.ImageStamped";
+	std::string topic = "/gazebo/default/iris_demo/iris_demo/gimbal_2d/tilt_link/camera/image";
+	//gazebo::gui::ImageView mainView(&window);
+	//
+	gazebo::gui::ViewFactory::RegisterAll();
+	gazebo::gui::TopicView *view = gazebo::gui::ViewFactory::NewView(msgType, topic, &window);
+	if (view)
+		view->show();
+	else
+		std::cout << "Unable to create viewer for message type[" << msgType << "]\n";
+	window.show();
+
+	//while (true)
+	//	gazebo::common::Time::MSleep(10);
+
+	std::cout << "Shutingdown\n";
+	return app.exec();
+
+}
